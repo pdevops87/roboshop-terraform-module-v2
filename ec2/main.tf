f1933757e0a23fdd2a33b6cee0bda6ad12e92962
Your Name <vemasani.pavani@gmail.com>
2026-01-28 17:30:12 +0530

    security group

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami  = var.ami
   instance_type = var.instance_type
-  security_groups = [var.vpc_sg]
+  vpc_security_group_ids = [var.vpc_sg]
   tags = {
     Name = var.component
   }
