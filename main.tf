1a115a04bf8ece452f37f60c1eb39fb0fc2bd61b
Your Name <vemasani.pavani@gmail.com>
2026-01-29 12:49:23 +0530

    one component

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ec2"{
-  for_each = var.components
   depends_on = [module.network]
   source = "./ec2"
+  for_each = var.components
   component = each.key
   ami = var.ami
   instance_type = var.instance_type
@@ -14,23 +14,14 @@ module "network"{
   env = var.env
 }
 
-module "route53"{
-  for_each = var.components
-  depends_on = [module.ec2]
-  source = "./route53"
-  components = each.key
-  type = "A"
-  zone_id = var.zone_id
-  privateIP = module.ec2[each.key].private_ip
-  env=var.env
-}
+# module "route53"{
+#   for_each = var.components
+#   source = "./route53"
+#   components = each.key
+#   type = "A"
+#   zone_id = var.zone_id
+#   privateIP = module.ec2[each.key].private_ip
+#   env=var.env
+# }
 
-module "ansible"{
-  depends_on = [module.ec2,module.route53]
-  for_each = var.components
-  source = "./ansible"
-  instanceId = module.ec2[each.key].instance
-  component = each.key
-  env = var.env
-  privateIP = module.ec2[each.key].private_ip
-}
+
