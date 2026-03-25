19cd9141a7d547217694ec5938b52739c6c1c2cb
Your Name <vemasani.pavani@gmail.com>
2026-03-25 21:50:01 +0530

    to run yaml locally

--- ec2/main.tf
+++ ec2/main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "instance" {
 #     inline = [
 #       "sudo dnf install python3.11-pip -y",
 #       "sudo pip3.11 install ansible",
-#       "ansible-pull -i localhost, -U https://github.com/pdevops87/roboshop-ansible-v4 roboshop.yaml -e component=${var.component} -e env=${var.env}"
+#       "ansible-pull -i localhost, -U https://github.com/pdevops87/roboshop-ansible-v4 runner.yaml -e component=${var.component} -e env=${var.env}"
 #     ]
 #   }
 # }
